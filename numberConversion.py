cccb1cfcd09f7a95fbed92e7511e84f76120721b
Mageswarikaruppasamy <132690569+Mageswarikaruppasamy@users.noreply.github.com>
2023-12-01 22:02:44 +0530

    Add files via upload

---
+++ numberConversion.py
@@ -0,0 +1,6 @@
+#Coding for number conversion
+num=int(input("Enter a number: "))
+print("Decimal value of",num,"=",float(num))  #decimal value
+print("Binary value of",num,"=",bin(num))  #binary value
+print("Octal value of",num,"=",oct(num)) #octal value
+print("Hexadecimal value of",num,"=",hex(num)) #hexadecimal value
